e1110f59777bbadb516803546924e9f80f5fe5a3
Mikhail <75259195+MikeRogovskiy@users.noreply.github.com>
2022-07-22 12:54:13 +0300

    Extract domain verbs

--- crudcrud/people.test.js
+++ crudcrud/people.test.js
@@ -7,6 +7,20 @@ async function fetchJSON(url, ...args) {
   return response.json();
 }
 
+function createPerson(data) {
+  return fetchJSON(`/people`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+function readPerson(id) {
+  return fetchJSON(`/people/${id}`);
+}
+
 describe("CrudCrud: People", () => {
   // Cоздать фейковые данные
   it("can create a person", async () => {
@@ -14,16 +28,7 @@ describe("CrudCrud: People", () => {
     const age = Math.ceil(Math.random() * 100);
     // отправить запрос на создание с этими данными
 
-    const createResponseData = await fetchJSON(`/people`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        age,
-      }),
-    });
+    const createResponseData = await createPerson({ age, name });
     // проверить ответ от запроса(что там есть наши данные)
     expect(createResponseData).toEqual(
       expect.objectContaining({
@@ -33,9 +38,7 @@ describe("CrudCrud: People", () => {
       })
     );
     // отправить запрос на чтение созданной персоны
-    const readPersonResponseData = await fetchJSON(
-      `/people/${createResponseData._id}`
-    );
+    const readPersonResponseData = readPerson(createResponseData._id);
     // проверить ответ на данные, которые мы сгенерировали
     expect(readPersonResponseData).toEqual({
       age,
