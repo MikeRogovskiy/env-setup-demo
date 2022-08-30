01eabd5135e1dc80cf4c2de72baede652ccba3d6
Mikhail <75259195+MikeRogovskiy@users.noreply.github.com>
2022-08-30 18:01:55 +0300

    wdtest renders button which appears on input and disappears on clear

--- wd.tests/wdtest.test.js
+++ wd.tests/wdtest.test.js
@@ -1,7 +1,17 @@
 const { remote } = require("webdriverio");
 
+function sleep(x) {
+  // eslint-disable-next-line no-promise-executor-return
+  return new Promise((resolve) => setTimeout(resolve, x));
+}
+jest.setTimeout(30000);
 describe("List of paragraphs", () => {
   let browser;
+  let input;
+  let button;
+  // beforeAll( async () => {
+  // });
+
   beforeEach(async () => {
     browser = await remote({
       capabilities: {
@@ -12,9 +22,13 @@ describe("List of paragraphs", () => {
     await browser.$("input").waitForExist({
       timeout: 5000,
     });
+    input = await browser.$("input");
+    button = await browser.$("button");
   });
+
   afterEach(async () => {
     await browser.deleteSession();
+    await sleep(1000);
   });
 
   async function getParagraphs() {
@@ -23,9 +37,54 @@ describe("List of paragraphs", () => {
     return pListText;
   }
 
+  async function clickButton() {
+    // eslint-disable-next-line no-return-await
+    return await button.click();
+  }
+
+  async function isInputVisible() {
+    // eslint-disable-next-line no-return-await
+    return await input.isDisplayed();
+  }
+
+  async function getInputValue() {
+    // eslint-disable-next-line no-return-await
+    return await input.getValue();
+  }
+  async function type(str) {
+    // eslint-disable-next-line no-return-await
+    return await input.setValue(str);
+  }
+  async function isButtonHidden() {
+    // eslint-disable-next-line no-return-await
+    return !(await button.isDisplayed());
+  }
+
   it("renders 3 paragraphs and input", async () => {
     expect(await getParagraphs()).toEqual(["1", "2", "3"]);
     // поле ввода
-    // expect(el.querySelectorAll("input").length).toBe(1);
+    expect(await isInputVisible()).toBe(true);
+  });
+
+  it("adds new paragraph on button click", async () => {
+    await type("123");
+
+    await clickButton();
+
+    expect((await getParagraphs()).length).toBe(4);
+
+    expect((await getParagraphs())[0]).toBe("123");
+    expect(await isButtonHidden()).toBe(true);
+    expect(await getInputValue()).toBe("");
+  });
+
+  it("renders button which appears on input and disappears on clear", async () => {
+    // ввести текст
+    await type("123");
+
+    expect(await isButtonHidden()).toBe(false);
+
+    await type("");
+    expect(await isButtonHidden()).toBe(false);
   });
 });
